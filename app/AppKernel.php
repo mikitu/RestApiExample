b7cdf82ff17d447efbd4e865adde97dc027be206
Mihai Bucse <mbucse@mediamath.com>
2016-03-12 09:24:39 +0000

    add FOSRestBundle and JMSSerializerBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new ApiBundle\ApiBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
