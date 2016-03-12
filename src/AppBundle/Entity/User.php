30dccaed4d84a94eaf2528589aa942c0a4e08e2f
Mihai Bucse <mbucse@mediamath.com>
2016-03-12 08:56:44 +0000

    add and configure FOSUserBundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mbucse
+ * Date: 12/03/2016
+ * Time: 08:47
+ */
+
+namespace AppBundle\Entity;
+
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table("users")
+ * @ORM\Entity
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
